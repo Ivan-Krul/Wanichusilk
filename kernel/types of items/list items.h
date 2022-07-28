104b8529167e9e5c32ec049d5d85063dff61719b
Ivan-Krul <kiwiivanya128@gmail.com>
2022-07-28 10:09:52 +0300

    add more items for example

--- kernel/types of items/list items.h
+++ kernel/types of items/list items.h
@@ -3,7 +3,18 @@
 
 class sandwich : public edible_item {
 	const static int _cheal = 5;
-	
 public:
 	sandwich() : edible_item(name("sandwich"), "sandwich (heal " + std::to_string(_cheal) + ") - simple combination of a bread, salad, hamand souces, borrowed from Heavy", _cheal) {}
+};
+
+class banana : public edible_item {
+	const static int _cheal = 3;
+public:
+	banana() : edible_item(name("banana"), "banana (heal " + std::to_string(_cheal) + ") - fruit for fitness, filled potassium", _cheal) {}
+};
+
+class helmet : public equip_item {
+	const static int _cequip = 4;
+public:
+	helmet() : equip_item(name("helmet"), "helmet (def " + std::to_string(_cequip) + ") - very huge and very massive", _cequip) {}
 };
